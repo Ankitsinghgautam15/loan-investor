4993dafb3bfb859826071d403b42d8cb9529aee5
Ankitsinghgautam15 <44620143+Ankitsinghgautam15@users.noreply.github.com>
2019-11-08 12:59:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,47 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+let nCount = selector => {
+  $(selector).each(function () {
+    $(this)
+      .animate({
+        Counter: $(this).text()
+      }, {
+        // 
+        duration: 4000,
+        
+        easing: "swing",
+        
+        step: function (value) {
+          $(this).text(Math.ceil(value));
+        }
+      });
+  });
+};
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+let a = 0;
+$(window).scroll(function () {
+  // The .offset() method allows us to retrieve the current position of an element  relative to the document
+  let oTop = $(".numbers").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() >= oTop) {
+    a++;
+    nCount(".rect > h1");
+  }
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+
+
+/**
+ *
+ *  sticky navigation
+ *
+ */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+ 
+  let oTop = $(".section-2").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
 });
